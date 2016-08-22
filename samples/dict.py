58cadd2e04f5caab213e3aed10ef6810b5b34afc
weiguiping <weiguiping@globalroam.com>
2016-08-22 18:07:29 +0800

    类型java的map的字典

---
+++ samples/dict.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+# -*-coding:utf8-*-
+
+x = {'a': 'aaa', 'b': 'bbb', 'c': 12}
+for key in x:
+    print("Key is %s and value is %s" % (key, x[key]))
+
+''' 
+知识点:  
+* 将他当Java的Map来用即可.
+'''
