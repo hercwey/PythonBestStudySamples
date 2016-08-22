443468a6fd91ba48442f2a60a480bf78d5843002
weiguiping <weiguiping@globalroam.com>
2016-08-22 15:55:47 +0800

    hello world

---
+++ samples/hello_world.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+# -*-coding:utf8-*-
+s1 = input("Input your name:")
+print("Hello, %s" % s1)
+
+''' 
+知识点:  
+* input("某字符串")函数:显示"某字符串",并等待用户输入.     
+* print()函数:如何打印.     
+* 如何应用中文     
+* 如何用多行注释 
+'''
+
