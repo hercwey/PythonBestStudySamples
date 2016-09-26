d9c8bbf9cf4bdb78330078821b5e62bf652d369d
weiguiping <weiguiping@globalroam.com>
2016-09-26 11:58:38 +0800

    deep copy

---
+++ samples/basic/@example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@version: 1.0
+@author: Hercwey
+@license: Apache Licence 
+@contact: 29053631@qq.com
+@site: 
+@software: PyCharm Community Edition
+@file: @example.py
+@time: 2016/9/26 11:44
+"""
