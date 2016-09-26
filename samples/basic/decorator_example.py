b0a6d067e197a3920974a57a5e1dfe6cfa4c79fd
weiguiping <weiguiping@globalroam.com>
2016-09-26 18:38:59 +0800

    decorator

--- samples/basic/decorator_example.py
+++ samples/basic/decorator_example.py
@@ -102,5 +102,41 @@ def square_sum(a, b):
 def square_diff(a, b):
     return a**2 - b**2
 
-print(square_sum(3, 4))
+print(square_sum(3, 4)) # 相当于square_sum = pre_str('^_^') (square_sum)
 print(square_diff(3, 4))
+
+print "-----------------------------------------------------------"
+
+"""
+装饰类
+装饰器可以接收一个类，并返回一个类，从而起到加工类的效果。
+"""
+def decorator(aClass):
+    class newClass:
+        def __init__(self, age):
+            self.total_display = 0
+            self.wrapped = aClass(age)
+        def display(self):
+            self.total_display += 1
+            print("total display", self.total_display)
+            self.wrapped.display()
+    return newClass
+
+@decorator
+class Bird:
+    def __init__(self, age):
+        self.age = age
+    def display(self):
+        print("My age is",self.age)
+
+eagleLord = Bird(5)
+for i in range(3):
+    eagleLord.display()
+
+"""
+总结
+
+装饰器的核心作用是name binding。这种语法是Python多编程范式的又一个体现。
+大部分Python用户都不怎么需要定义装饰器，但有可能会使用装饰器。
+鉴于装饰器在Python项目中的广泛使用，了解这一语法是非常有益的。
+"""
