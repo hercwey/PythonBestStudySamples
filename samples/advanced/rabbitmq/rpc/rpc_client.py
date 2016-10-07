3a3a7b9a91034bcb23830e46407ed7eef92e3c9f
hercwey <bernwey@gmail.com>
2016-10-07 09:44:13 +0800

    Default

--- samples/advanced/rabbitmq/rpc/rpc_client.py
+++ samples/advanced/rabbitmq/rpc/rpc_client.py
@@ -43,3 +43,14 @@ fibonacci_rpc = FibonacciRpcClient()
 print(" [x] Requesting fib(30)")
 response = fibonacci_rpc.call(30)
 print(" [.] Got %r" % response)
+
+"""
+RPC如此工作:
+
+    当客户端启动的时候，它创建一个匿名独享的回调队列。
+    在RPC请求中，客户端发送带有两个属性的消息：一个是设置回调队列的 reply_to 属性，另一个是设置唯一值的 correlation_id 属性。
+    将请求发送到一个 rpc_queue 队列中。
+    RPC工作者（又名：服务器）等待请求发送到这个队列中来。当请求出现的时候，它执行他的工作并且将带有执行结果的消息发送给reply_to字段指定的队列。
+    客户端等待回调队列里的数据。当有消息出现的时候，它会检查correlation_id属性。如果此属性的值与请求匹配，将它返回给应用。
+
+"""
